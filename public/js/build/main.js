0d80a07a2ef0e195b40b4cbfad5b8a5267dcf494
Jugal Manjeshwar <jugalm9@gmail.com>
2015-05-03 12:55:11 -0400

    Added pagination, thumbnail lazy loading and result status display.

--- public/js/build/main.js
+++ public/js/build/main.js
@@ -1,25 +1,174 @@
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var CompanyThumbnail = React.createClass({displayName: "CompanyThumbnail",
+	getInitialState: function() {
+		return {
+			status: 'loading'
+		}
+	},
+
+	componentWillMount: function(){
+		if(!this.props.src) {
+			this.handleError();
+		}
+	},
+
+	componentWillReceiveProps: function(nextProps) {
+		if(nextProps.src !== this.props.src) {
+			var imgNode = this.refs.img.getDOMNode();
+			imgNode.style.width = this.props.maxSize + 'px';
+			imgNode.style.height = this.props.maxSize + 'px';
+			imgNode.style.marginTop = null;
+			imgNode.style.marginLeft = null;
+			this.setState({
+				status: 'loading'
+			});			
+		}
+	},	
+
+	handleLoad: function(){
+		var imgNode = this.refs.img.getDOMNode();
+		if(imgNode.width > imgNode.height) {
+			imgNode.style.width = this.props.maxSize + 'px';
+			imgNode.style.height = 'auto';
+			imgNode.style.marginTop = (this.props.maxSize / 2 - (this.props.maxSize * imgNode.height / imgNode.width) / 2) + 'px';
+		}
+		else {
+			imgNode.style.height = this.props.maxSize + 'px';
+			imgNode.style.width = 'auto';
+			imgNode.style.marginLeft = (this.props.maxSize / 2 - (this.props.maxSize * imgNode.width / imgNode.height) / 2) + 'px';
+		}
+		this.setState({
+			status: 'loaded'
+		});
+	},
+
+	handleError: function(){
+		this.setState({
+			status: 'failed'
+		});
+	},
+
+	render: function() {
+	    var className = 'thumb-' + this.state.status;
+	    return (
+	    	React.createElement("div", {className: className}, 
+		    	React.createElement("div", {className: "spinner"}), 
+		      	React.createElement("img", {src: this.props.src, alt: this.props.alt, onLoad: this.handleLoad, onError: this.handleError, ref: "img"})
+		     )
+	    );
+	}
+});
+
 var CompanyRow = React.createClass({displayName: "CompanyRow",
     render: function() {
+    	var maxThumbSize = 40;
         return (
                 React.createElement("li", {className: "company-list-item row"}, 
-                	React.createElement("div", {className: "company-item-img col"}, React.createElement("span", {className: "company-item-img-hlp"}), React.createElement("img", {src: this.props.company.i})), 
+                	React.createElement("div", {className: "company-item-img col"}, 
+                		React.createElement(CompanyThumbnail, {src: this.props.company.i, maxSize: maxThumbSize})
+                	), 
                 	React.createElement("div", {className: "company-name col"}, React.createElement("a", {href: '#' + this.props.company.p}, this.props.company.n))
                 )
         );
     }
 });
 
+function pager(page) {
+	if(page.numPages < 2) 
+		return;
+
+	var pageLinks = [];
+	if (page.currentPage > 1) {
+		pageLinks.push(React.createElement("a", {className: "page-link col", onClick: page.handleClick(page.currentPage - 1)}, "‹"))
+	}
+
+	var startPage = page.currentPage - (page.currentPage > 1 ? 6 : 7);
+	var endPage = page.currentPage + 6;
+	if (startPage <= 0) {
+	    endPage -= (startPage - 1);
+	    startPage = 1;
+	}
+	if (endPage > page.numPages) {
+    	endPage = page.numPages;
+	}
+
+	for(var i = startPage; i <= endPage; i++) {
+		if(page.currentPage == i) {
+			pageLinks.push(React.createElement("a", {className: "page-link page-curr col"}, i))	
+		}
+		else {
+			pageLinks.push(React.createElement("a", {className: "page-link col", onClick: page.handleClick(i)}, i))
+		}		
+	}
+
+	if (page.currentPage < page.numPages) {
+		pageLinks.push(React.createElement("span", {className: "page-link col-right", onClick: page.handleClick(page.currentPage + 1)}, "›"))
+	}
+	return React.createElement("div", {className: "pager row"}, pageLinks)
+}
+
 var CompanyList = React.createClass({displayName: "CompanyList",
+	getInitialState: function() {
+		return {
+			currentPage: 1
+		}
+	},
+
+	getDefaultProps: function() {
+		return {
+			pageSize: 10
+		}
+	},
+	
+	componentWillReceiveProps: function(nextProps) {
+    	this.setState({
+      		currentPage: 1
+    	})
+  	},
+
+	getPage: function() {
+	   	var start = this.props.pageSize * (this.state.currentPage - 1);
+	    var end = start + this.props.pageSize;
+
+	    return {
+	      	currentPage: this.state.currentPage, 
+	      	companies: this.props.companies.slice(start, end), 
+	      	numPages: this.getNumPages(), 
+	      	handleClick: function(pageNum) {
+	        	return function() { 
+	        		this.handlePageChange(pageNum);
+	        	}.bind(this)
+	      	}.bind(this)
+	    }
+	},
+
+	getNumPages: function() {
+	    var numPages = Math.floor(this.props.companies.length / this.props.pageSize);
+	    if (this.props.companies.length % this.props.pageSize > 0) {
+	      	numPages++
+	    }
+	    return numPages;
+	},
+
+	handlePageChange: function(pageNum) {
+	    this.setState({currentPage: pageNum});
+	},
+
     render: function() {
-        var rows = [],
-        	to = Math.min(this.props.companies.length, 10);
-        for( var i = 0; i < to; i++) {
-        	var company = this.props.companies[i];
-        	rows.push(React.createElement(CompanyRow, {company: company}));
-        }
+    	var page = this.getPage();
+        var rows = page.companies.map(function(company) {
+        	return React.createElement(CompanyRow, {company: company})
+        });
         return (
-        	React.createElement("ul", {className: "company-list"}, 
-        		rows
+        	React.createElement("div", null, 
+        		React.createElement("div", {className: "result-status"}, this.props.companies.length > 0 ? numberWithCommas(this.props.companies.length) + ' results' : this.props.query === '' ? 'Type a query' : 'Your query \'' + this.props.query + '\' returned no results'), 
+	        	React.createElement("ul", {className: "company-list"}, 
+	        		rows
+	        	), 
+	        	pager(page)
         	)
         );
     }
@@ -46,7 +195,8 @@ var SearchBar = React.createClass({displayName: "SearchBar",
 var FilterableCompanyList = React.createClass({displayName: "FilterableCompanyList",
 	getInitialState: function() {
 		return {
-			companies: []
+			companies: [],
+			query: ''
 		}
 	},
 
@@ -56,7 +206,7 @@ var FilterableCompanyList = React.createClass({displayName: "FilterableCompanyLi
 	      	url: queryUrl,
 	      	dataType: 'json',
 	      	success: function(data) {
-	        	this.setState({companies: data});
+	        	this.setState({companies: data, query: q});
 	      	}.bind(this),
 	      	error: function(xhr, status, err) {
 	        	console.error(queryUrl, status, err.toString());
@@ -65,8 +215,8 @@ var FilterableCompanyList = React.createClass({displayName: "FilterableCompanyLi
 	},
 
 	handleOnQuery: function(q) {
-		if(q.length < 2) {
-			this.setState({companies: []});
+		if(q.length < 3) {
+			this.setState({companies: [], query: ''});
 		}
 		else {
 			this.loadCompaniesFromServer(q);
@@ -77,17 +227,22 @@ var FilterableCompanyList = React.createClass({displayName: "FilterableCompanyLi
         return (
         	React.createElement("div", null, 
 	            React.createElement("div", {className: "header row"}, 
-	            	React.createElement("h1", {className: "logo col"}, "Investup"), 
-	                React.createElement(SearchBar, {
-	                	className: "col", 
-	                	onQuery: this.handleOnQuery}
-	                )
-	            ), 
-	            React.createElement("div", {className: "list-wrapper"}, 
-		            React.createElement(CompanyList, {
-		            	companies: this.state.companies}
+	            	React.createElement("div", {className: "wrapper"}, 
+		            	React.createElement("h1", {className: "logo col"}, "Investup"), 
+		                React.createElement(SearchBar, {
+		                	className: "col", 
+		                	onQuery: this.handleOnQuery}
+		                )
 		            )
-		        )
+	            ), 
+	           	React.createElement("div", {className: "wrapper"}, 
+		            React.createElement("div", {className: "list-wrapper"}, 
+			            React.createElement(CompanyList, {
+			            	companies: this.state.companies, 
+			            	query: this.state.query}
+			            )
+			        )
+			    )
 	        )
             
         );
--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,26 +1,175 @@
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+var CompanyThumbnail = React.createClass({
+	getInitialState: function() {
+		return {
+			status: 'loading'
+		}
+	},
+
+	componentWillMount: function(){
+		if(!this.props.src) {
+			this.handleError();
+		}
+	},
+
+	componentWillReceiveProps: function(nextProps) {
+		if(nextProps.src !== this.props.src) {
+			var imgNode = this.refs.img.getDOMNode();
+			imgNode.style.width = this.props.maxSize + 'px';
+			imgNode.style.height = this.props.maxSize + 'px';
+			imgNode.style.marginTop = null;
+			imgNode.style.marginLeft = null;
+			this.setState({
+				status: 'loading'
+			});			
+		}
+	},	
+
+	handleLoad: function(){
+		var imgNode = this.refs.img.getDOMNode();
+		if(imgNode.width > imgNode.height) {
+			imgNode.style.width = this.props.maxSize + 'px';
+			imgNode.style.height = 'auto';
+			imgNode.style.marginTop = (this.props.maxSize / 2 - (this.props.maxSize * imgNode.height / imgNode.width) / 2) + 'px';
+		}
+		else {
+			imgNode.style.height = this.props.maxSize + 'px';
+			imgNode.style.width = 'auto';
+			imgNode.style.marginLeft = (this.props.maxSize / 2 - (this.props.maxSize * imgNode.width / imgNode.height) / 2) + 'px';
+		}
+		this.setState({
+			status: 'loaded'
+		});
+	},
+
+	handleError: function(){
+		this.setState({
+			status: 'failed'
+		});
+	},
+
+	render: function() {
+	    var className = 'thumb-' + this.state.status;
+	    return (
+	    	<div className={className}>
+		    	<div className="spinner"></div>
+		      	<img src={this.props.src} alt={this.props.alt} onLoad={this.handleLoad} onError={this.handleError} ref="img"/>
+		     </div>
+	    );
+	}
+});
+
 var CompanyRow = React.createClass({
     render: function() {
+    	var maxThumbSize = 40;
         return (
                 <li className="company-list-item row">
-                	<div className="company-item-img col"><span className="company-item-img-hlp"></span><img src={this.props.company.i}/></div>
+                	<div className="company-item-img col">
+                		<CompanyThumbnail src={this.props.company.i} maxSize={maxThumbSize} />
+                	</div>
                 	<div className="company-name col"><a href={'#' + this.props.company.p}>{this.props.company.n}</a></div>
                 </li>
         );
     }
 });
 
+function pager(page) {
+	if(page.numPages < 2) 
+		return;
+
+	var pageLinks = [];
+	if (page.currentPage > 1) {
+		pageLinks.push(<a className="page-link col" onClick={page.handleClick(page.currentPage - 1)}>‹</a>)
+	}
+
+	var startPage = page.currentPage - (page.currentPage > 1 ? 6 : 7);
+	var endPage = page.currentPage + 6;
+	if (startPage <= 0) {
+	    endPage -= (startPage - 1);
+	    startPage = 1;
+	}
+	if (endPage > page.numPages) {
+    	endPage = page.numPages;
+	}
+
+	for(var i = startPage; i <= endPage; i++) {
+		if(page.currentPage == i) {
+			pageLinks.push(<a className="page-link page-curr col">{i}</a>)	
+		}
+		else {
+			pageLinks.push(<a className="page-link col" onClick={page.handleClick(i)}>{i}</a>)
+		}		
+	}
+
+	if (page.currentPage < page.numPages) {
+		pageLinks.push(<span className="page-link col-right" onClick={page.handleClick(page.currentPage + 1)}>›</span>)
+	}
+	return <div className="pager row">{pageLinks}</div>
+}
+
 var CompanyList = React.createClass({
+	getInitialState: function() {
+		return {
+			currentPage: 1
+		}
+	},
+
+	getDefaultProps: function() {
+		return {
+			pageSize: 10
+		}
+	},
+	
+	componentWillReceiveProps: function(nextProps) {
+    	this.setState({
+      		currentPage: 1
+    	})
+  	},
+
+	getPage: function() {
+	   	var start = this.props.pageSize * (this.state.currentPage - 1);
+	    var end = start + this.props.pageSize;
+
+	    return {
+	      	currentPage: this.state.currentPage, 
+	      	companies: this.props.companies.slice(start, end), 
+	      	numPages: this.getNumPages(), 
+	      	handleClick: function(pageNum) {
+	        	return function() { 
+	        		this.handlePageChange(pageNum);
+	        	}.bind(this)
+	      	}.bind(this)
+	    }
+	},
+
+	getNumPages: function() {
+	    var numPages = Math.floor(this.props.companies.length / this.props.pageSize);
+	    if (this.props.companies.length % this.props.pageSize > 0) {
+	      	numPages++
+	    }
+	    return numPages;
+	},
+
+	handlePageChange: function(pageNum) {
+	    this.setState({currentPage: pageNum});
+	},
+
     render: function() {
-        var rows = [],
-        	to = Math.min(this.props.companies.length, 10);
-        for( var i = 0; i < to; i++) {
-        	var company = this.props.companies[i];
-        	rows.push(<CompanyRow company={company} />);
-        }
+    	var page = this.getPage();
+        var rows = page.companies.map(function(company) {
+        	return <CompanyRow company={company} />
+        });
         return (
-        	<ul className="company-list">
-        		{rows}
-        	</ul>
+        	<div>
+        		<div className="result-status">{this.props.companies.length > 0 ? numberWithCommas(this.props.companies.length) + ' results' : this.props.query === '' ? 'Type a query' : 'Your query \'' + this.props.query + '\' returned no results'}</div>
+	        	<ul className="company-list">
+	        		{rows}
+	        	</ul>
+	        	{pager(page)}
+        	</div>
         );
     }
 });
@@ -46,7 +195,8 @@ var SearchBar = React.createClass({
 var FilterableCompanyList = React.createClass({
 	getInitialState: function() {
 		return {
-			companies: []
+			companies: [],
+			query: ''
 		}
 	},
 
@@ -56,7 +206,7 @@ var FilterableCompanyList = React.createClass({
 	      	url: queryUrl,
 	      	dataType: 'json',
 	      	success: function(data) {
-	        	this.setState({companies: data});
+	        	this.setState({companies: data, query: q});
 	      	}.bind(this),
 	      	error: function(xhr, status, err) {
 	        	console.error(queryUrl, status, err.toString());
@@ -65,8 +215,8 @@ var FilterableCompanyList = React.createClass({
 	},
 
 	handleOnQuery: function(q) {
-		if(q.length < 2) {
-			this.setState({companies: []});
+		if(q.length < 3) {
+			this.setState({companies: [], query: ''});
 		}
 		else {
 			this.loadCompaniesFromServer(q);
@@ -77,17 +227,22 @@ var FilterableCompanyList = React.createClass({
         return (
         	<div>
 	            <div className="header row">
-	            	<h1 className="logo col">Investup</h1>
-	                <SearchBar
-	                	className="col"
-	                	onQuery={this.handleOnQuery}
-	                />
+	            	<div className="wrapper">
+		            	<h1 className="logo col">Investup</h1>
+		                <SearchBar
+		                	className="col"
+		                	onQuery={this.handleOnQuery}
+		                />
+		            </div>
 	            </div>
-	            <div className="list-wrapper">
-		            <CompanyList
-		            	companies={this.state.companies}
-		            />
-		        </div>
+	           	<div className="wrapper">
+		            <div className="list-wrapper">
+			            <CompanyList
+			            	companies={this.state.companies}
+			            	query={this.state.query}
+			            />
+			        </div>
+			    </div>
 	        </div>
             
         );
